cd743a84d4849fe888daf1ba97de26724f4b24e6
Achess <jealtzo26@gmail.com>
2025-04-24 21:25:15 -0600

    Fix routes in firewall

--- firewall/setup-firewall.sh
+++ firewall/setup-firewall.sh
@@ -35,19 +35,19 @@ table inet firewall {
     ct state established,related accept
 
     # Subred B → A, C, D, E
-    ip saddr 192.168.50.0/27 ip daddr {192.168.40.0/24, 192.168.60.0/24, 192.168.70.0/24, 192.168.80.0/24} accept
+    ip saddr 192.168.50.0/27 ip daddr {192.168.40.0/24, 192.168.10.0/24, 192.168.20.0/24, 192.168.30.0/24} accept
 
     # Subred C → E
-    ip saddr 192.168.60.0/24 ip daddr 192.168.80.0/24 accept
+    ip saddr 192.168.10.0/24 ip daddr 192.168.30.0/24 accept
 
     # Subred D → C
-    ip saddr 192.168.70.0/24 ip daddr 192.168.60.0/24 accept
+    ip saddr 192.168.20.0/24 ip daddr 192.168.10.0/24 accept
 
     # Subred E → C
-    ip saddr 192.168.80.0/24 ip daddr 192.168.60.0/24 accept
+    ip saddr 192.168.30.0/24 ip daddr 192.168.10.0/24 accept
 
-    # Permitir acceso a IP específica en subred A (por ejemplo 192.168.40.100)
-    ip daddr 192.168.40.100 accept
+    # Permitir Home page pública en subred A
+    ip daddr 192.168.40.10 accept
 
     # LOG conexiones aceptadas
     ip saddr 192.168.0.0/16 ip daddr 192.168.0.0/16 log prefix "ACCEPTED: " flags all level info accept
